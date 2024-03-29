a9d25e1a7f35539dbe2f97f89a2654821a02e010
amr-hc <amro7275@gmail.com>
2024-03-29 16:11:14 +0200

    object id for teacher

--- Midelwares/validations/teacherValidation.js
+++ Midelwares/validations/teacherValidation.js
@@ -7,7 +7,7 @@ exports.insert=[
 ]
 
 exports.update=[
-     body("_id").optional().isInt().withMessage("id Must be an integer"),
+     body("_id").isMongoId().withMessage("id Must be an MongoId"),
      body("fullname").optional().matches(/^[a-zA-Z][a-zA-Z][a-zA-Z]+(\s[a-zA-Z][a-zA-Z][a-zA-Z]+)*$/).withMessage("fullname Must be string").isLength({min:3}).withMessage("fullname Must be at least 3 characters"),
      body("password").custom((value)=>{if(value === undefined) return true;}).withMessage("cant update password here"),
      body("email").optional().isEmail().withMessage("Must be a valid email"),
--- Model/teacherModel.js
+++ Model/teacherModel.js
@@ -1,15 +1,12 @@
 const mongoose = require('mongoose');
-const sequence = require("mongoose-sequence")(mongoose);
+
 
 const schema = new mongoose.Schema({
-    _id: Number,
     fullname: String,
     email: {type: String , unique: true},
     password: {type:String , select : false},
     image: String,
 });
-
-schema.plugin(sequence,{id: 'teacher_counter'});
 
 
 module.exports = mongoose.model("teachers", schema);
